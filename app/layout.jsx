40ab1f901f05a63ca54beb8c0e99fb8e3137a4ec
spectr-e <devs.josia@gmail.com>
2024-05-21 17:24:54 +0300

    feat: Add app metadata in the main layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,12 @@
 import '@/assets/styles/globals.css'
 
+export const metadata = {
+  title: 'Kodisha | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords:
+    'rental, find rentals, rent in kenya, properties in kenya, cheapest houses in kenya, luxury homes',
+}
+
 const MainLayout = ({ children }) => {
   return (
     <html lang='en'>
