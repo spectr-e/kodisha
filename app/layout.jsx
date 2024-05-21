0fec2a1f97c5be0e3ae62dda32608793263e9880
spectr-e <devs.josia@gmail.com>
2024-05-21 17:14:31 +0300

    feat(app): Create the main layout of the app

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MainLayout = () => {
+  return (
+    <html lang='en'>
+      <body>
+        <div>MainLayout</div>
+      </body>
+    </html>
+  )
+}
+
+export default MainLayout
