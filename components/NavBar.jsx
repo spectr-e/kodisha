32e0d50dc0eea68b5a832f488bee01e4c3301329
spectr-e <devs.josia@gmail.com>
2024-05-21 18:27:24 +0300

    fix(NavBar): Add and import react icons into project

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { logoWhite, profile } from '@/assets/images'
 import Image from 'next/image'
 import Link from 'next/link'
+import { FaGoogle } from 'react-icons/fa'
 
 const NavBar = () => {
   return (
@@ -73,7 +74,7 @@ const NavBar = () => {
           <div className='hidden md:block md:ml-6'>
             <div className='flex items-center'>
               <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-                <i className='fa-brands fa-google text-white mr-2'></i>
+                <FaGoogle className='text-white mr-2' />
                 <span>Login or Register</span>
               </button>
             </div>
@@ -191,7 +192,7 @@ const NavBar = () => {
             Add Property
           </Link>
           <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4'>
-            <i className='fa-brands fa-google mr-2'></i>
+            <FaGoogle />
             <span>Login or Register</span>
           </button>
         </div>
