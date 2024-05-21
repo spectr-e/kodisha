54a6edfa17333486f74232cacbeef03b16a768d7
spectr-e <devs.josia@gmail.com>
2024-05-21 18:51:42 +0300

    fix(NavBar): Add conditional rendering on protected routes based on login

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -9,6 +9,7 @@ import { FaGoogle } from 'react-icons/fa'
 const NavBar = () => {
   const [openMobileMenu, setOpenMobileMenu] = useState(false)
   const [openProfile, setOpenProfile] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(false)
 
   const pathName = usePathname()
 
@@ -74,14 +75,16 @@ const NavBar = () => {
                 >
                   Properties
                 </Link>
-                <Link
-                  href='/properties/add'
-                  className={`${
-                    pathName === '/properties/add' ? 'bg-black' : null
-                  } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
-                >
-                  Add Property
-                </Link>
+                {loggedIn && (
+                  <Link
+                    href='/properties/add'
+                    className={`${
+                      pathName === '/properties/add' ? 'bg-black' : null
+                    } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                  >
+                    Add Property
+                  </Link>
+                )}
               </div>
             </div>
           </div>
@@ -97,97 +100,99 @@ const NavBar = () => {
           </div>
 
           {/* <!-- Right Side Menu (Logged In) --> */}
-          <div className='absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0'>
-            {/* <!-- Notifications button --> */}
-            <Link href='/notifications' className='relative group'>
-              <button
-                type='button'
-                className='relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-              >
-                <span className='absolute -inset-1.5'></span>
-                <span className='sr-only'>View notifications</span>
-                <svg
-                  className='h-6 w-6'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth='1.5'
-                  stroke='currentColor'
-                  aria-hidden='true'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0'
-                  />
-                </svg>
-              </button>
-              <span className='absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full'>
-                2
-                {/* <!-- Replace with the actual number of notifications --> */}
-              </span>
-            </Link>
-
-            {/* <!-- Profile dropdown button --> */}
-            <div className='relative ml-3'>
-              <div>
+          {loggedIn && (
+            <div className='absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0'>
+              {/* <!-- Notifications button --> */}
+              <Link href='/notifications' className='relative group'>
                 <button
-                  onClick={() => setOpenProfile((prev) => !prev)}
                   type='button'
-                  className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-                  id='user-menu-button'
-                  aria-expanded='false'
-                  aria-haspopup='true'
+                  className='relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
                 >
                   <span className='absolute -inset-1.5'></span>
-                  <span className='sr-only'>Open user menu</span>
-                  <Image
-                    className='h-8 w-8 rounded-full'
-                    src={profile}
-                    alt=''
-                  />
+                  <span className='sr-only'>View notifications</span>
+                  <svg
+                    className='h-6 w-6'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth='1.5'
+                    stroke='currentColor'
+                    aria-hidden='true'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0'
+                    />
+                  </svg>
                 </button>
-              </div>
+                <span className='absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full'>
+                  2
+                  {/* <!-- Replace with the actual number of notifications --> */}
+                </span>
+              </Link>
 
-              {/* <!-- Profile dropdown --> */}
-              {openProfile && (
-                <div
-                  id='user-menu'
-                  className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
-                  role='menu'
-                  aria-orientation='vertical'
-                  aria-labelledby='user-menu-button'
-                  tabIndex='-1'
-                >
-                  <Link
-                    href='/profile'
-                    className='block px-4 py-2 text-sm text-gray-700'
-                    role='menuitem'
-                    tabIndex='-1'
-                    id='user-menu-item-0'
-                  >
-                    Your Profile
-                  </Link>
-                  <Link
-                    href='/properties/saved'
-                    className='block px-4 py-2 text-sm text-gray-700'
-                    role='menuitem'
-                    tabIndex='-1'
-                    id='user-menu-item-2'
-                  >
-                    Saved Properties
-                  </Link>
+              {/* <!-- Profile dropdown button --> */}
+              <div className='relative ml-3'>
+                <div>
                   <button
-                    className='block px-4 py-2 text-sm text-gray-700'
-                    role='menuitem'
-                    tabIndex='-1'
-                    id='user-menu-item-2'
+                    onClick={() => setOpenProfile((prev) => !prev)}
+                    type='button'
+                    className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+                    id='user-menu-button'
+                    aria-expanded='false'
+                    aria-haspopup='true'
                   >
-                    Sign Out
+                    <span className='absolute -inset-1.5'></span>
+                    <span className='sr-only'>Open user menu</span>
+                    <Image
+                      className='h-8 w-8 rounded-full'
+                      src={profile}
+                      alt=''
+                    />
                   </button>
                 </div>
-              )}
+
+                {/* <!-- Profile dropdown --> */}
+                {openProfile && (
+                  <div
+                    id='user-menu'
+                    className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+                    role='menu'
+                    aria-orientation='vertical'
+                    aria-labelledby='user-menu-button'
+                    tabIndex='-1'
+                  >
+                    <Link
+                      href='/profile'
+                      className='block px-4 py-2 text-sm text-gray-700'
+                      role='menuitem'
+                      tabIndex='-1'
+                      id='user-menu-item-0'
+                    >
+                      Your Profile
+                    </Link>
+                    <Link
+                      href='/properties/saved'
+                      className='block px-4 py-2 text-sm text-gray-700'
+                      role='menuitem'
+                      tabIndex='-1'
+                      id='user-menu-item-2'
+                    >
+                      Saved Properties
+                    </Link>
+                    <button
+                      className='block px-4 py-2 text-sm text-gray-700'
+                      role='menuitem'
+                      tabIndex='-1'
+                      id='user-menu-item-2'
+                    >
+                      Sign Out
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
 
@@ -211,14 +216,16 @@ const NavBar = () => {
             >
               Properties
             </Link>
-            <Link
-              href='/properties/add'
-              className={`${
-                pathName === '/properties/add' ? 'bg-gray-900 ' : null
-              } text-white block rounded-md px-3 py-2 text-base font-medium`}
-            >
-              Add Property
-            </Link>
+            {loggedIn && (
+              <Link
+                href='/properties/add'
+                className={`${
+                  pathName === '/properties/add' ? 'bg-gray-900 ' : null
+                } text-white block rounded-md px-3 py-2 text-base font-medium`}
+              >
+                Add Property
+              </Link>
+            )}
             <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4'>
               <FaGoogle className='text-white mr-2' />
               <span>Login or Register</span>
