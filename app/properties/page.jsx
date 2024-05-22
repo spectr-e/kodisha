fd7aacbd42482dfb4ea11526475331a81ef1fbd0
spectr-e <devs.josia@gmail.com>
2024-05-22 18:17:09 +0300

    fix(Properties): Import properties data & do a dummy display

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,17 @@
+import { properties } from '@/assets/data'
+
 const PropertiesPage = () => {
-  return <div>PropertiesPage</div>
+  return (
+    <section className='px-4 py-6'>
+      <div className='px-4 py-6 m-auto container-xl lg:container'>
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
+          {properties.map((prop, i) => (
+            <div key={i}>{prop.name}</div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default PropertiesPage
