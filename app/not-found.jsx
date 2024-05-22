9c8a39258998e324cd1a371aeecf27c2f2e7a1f5
spectr-e <devs.josia@gmail.com>
2024-05-22 19:26:31 +0300

    fix(NotFound): Import the default theme files

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,5 +1,28 @@
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>
+  return (
+    <section className='flex-grow min-h-screen bg-blue-50'>
+      <div className='container max-w-2xl py-24 m-auto'>
+        <div className='px-6 py-24 m-4 mb-4 bg-white border rounded-md shadow-md md:m-0'>
+          <div className='flex justify-center'>
+            <i className='text-yellow-400 fas fa-exclamation-triangle fa-5x text-8xl'></i>
+          </div>
+          <div className='text-center'>
+            <h1 className='mt-4 mb-2 text-3xl font-bold'>Page Not Found</h1>
+            <p className='mb-10 text-xl text-gray-500'>
+              The page you are looking for does not exist.
+            </p>
+            <a
+              href='/index.html'
+              className='px-6 py-4 font-bold text-white bg-blue-700 rounded hover:bg-blue-800'
+            >
+              Go Home
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='flex-grow'></div>
+    </section>
+  )
 }
 
 export default NotFoundPage
