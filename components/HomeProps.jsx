52e3df9378030f80751af3512cf728121377bee6
spectr-e <devs.josia@gmail.com>
2024-05-22 19:21:05 +0300

    fix(components/HomeProps): Display a view all properties button at the bottom

--- components/HomeProps.jsx
+++ components/HomeProps.jsx
@@ -1,5 +1,6 @@
 import { properties } from '@/assets/data'
 import { PropCard } from '.'
+import Link from 'next/link'
 
 const HomeProps = () => {
   const recentProps = properties
@@ -7,22 +8,33 @@ const HomeProps = () => {
     .slice(0, 3)
 
   return (
-    <section className='px-4 py-6'>
-      <div className='m-auto container-xl lg:container'>
-        <h2 className='mb-6 text-3xl font-bold text-center text-blue-500'>
-          Recent Properties
-        </h2>
-        <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
-          {recentProps === 0 ? (
-            <p>No Properties Found</p>
-          ) : (
-            recentProps.map((prop, i) => (
-              <PropCard key={prop.id} property={prop} />
-            ))
-          )}
+    <>
+      <section className='px-4 py-6'>
+        <div className='m-auto container-xl lg:container'>
+          <h2 className='mb-6 text-3xl font-bold text-center text-blue-500'>
+            Recent Properties
+          </h2>
+          <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
+            {recentProps === 0 ? (
+              <p>No Properties Found</p>
+            ) : (
+              recentProps.map((prop, i) => (
+                <PropCard key={prop.id} property={prop} />
+              ))
+            )}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <section className='max-w-lg px-6 m-auto my-10'>
+        <Link
+          href='/properties'
+          className='block px-6 py-4 text-center text-white bg-black rounded-xl hover:bg-gray-700'
+        >
+          View All Properties
+        </Link>
+      </section>
+    </>
   )
 }
 
