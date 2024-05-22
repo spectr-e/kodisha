4c3f94b4b6af98bf8705dd7d14f6d334d1fb12cc
spectr-e <devs.josia@gmail.com>
2024-05-22 17:56:38 +0300

    fix(Footer): Import defautl theme code with appropriate changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,32 @@
+import { logoBlue } from '@/assets/images'
+import Image from 'next/image'
+import Link from 'next/link'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className='py-4 mt-auto bg-gray-200'>
+      <div className='container flex flex-col items-center justify-between px-4 mx-auto md:flex-row'>
+        <div className='mb-4 md:mb-0'>
+          <Image src={logoBlue} alt='Logo' className='w-auto h-8' />
+        </div>
+        <div className='flex flex-wrap justify-center mb-4 md:justify-start md:mb-0'>
+          <ul className='flex space-x-4'>
+            <li>
+              <Link href='/properties'>Properties</Link>
+            </li>
+            <li>
+              <Link href='/terms'>Terms of Service</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className='mt-2 text-sm text-gray-500 md:mt-0'>
+            &copy; 2024 Kodisha. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
