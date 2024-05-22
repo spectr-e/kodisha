ec15c9586ae04c6f0550a85532255d3ffaa38e75
spectr-e <devs.josia@gmail.com>
2024-05-22 18:57:02 +0300

    fix(component/PropCard): Make the rate display dynamic

--- components/PropCard.jsx
+++ components/PropCard.jsx
@@ -9,6 +9,17 @@ import {
 } from 'react-icons/fa'
 
 const PropCard = ({ property }) => {
+  const getRate = () => {
+    const { rates } = property
+    if (rates.monthly) {
+      return `KES ${rates.monthly.toLocaleString()}/mo`
+    } else if (rates.weekly) {
+      return `KES ${rates.weekly.toLocaleString()}/wk`
+    } else if (rates.nightly) {
+      return `KES ${rates.nightly.toLocaleString()}/night`
+    }
+  }
+
   return (
     <div className='relative shadow-md rounded-xl'>
       <Image
@@ -25,7 +36,7 @@ const PropCard = ({ property }) => {
           <h3 className='text-xl font-bold'>{property.name}</h3>
         </div>
         <h3 className='absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right'>
-          KES {property.rates.monthly}/mo
+          {getRate()}
         </h3>
 
         <div className='flex justify-center gap-4 mb-4 text-gray-500'>
