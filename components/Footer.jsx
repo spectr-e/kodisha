933ffc2cf6477d70f7989e7ba62e68cd9b740838
spectr-e <devs.josia@gmail.com>
2024-05-22 18:05:10 +0300

    feat(assets): Bring in the properties data

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Footer = () => {
   const currentYr = new Date().getFullYear()
   return (
-    <footer className='py-4 mt-auto bg-gray-200'>
+    <footer className='py-4 mt-24 bg-gray-200'>
       <div className='container flex flex-col items-center justify-between px-4 mx-auto md:flex-row'>
         <div className='mb-4 md:mb-0'>
           <Image src={logoBlue} alt='Logo' className='w-auto h-8' />
