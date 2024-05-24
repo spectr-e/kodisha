eb308f4919625779bca2d6448f38dd3315701fe2
spectr-e <devs.josia@gmail.com>
2024-05-24 20:14:53 +0300

    fix(Property): Add function to fetch data from mongodb

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,32 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
+import { fetchProp } from '@/utils/requests'
 
-const PropertyPage = () => {}
+const PropertyPage = () => {
+  const { id } = useParams()
+  const [property, setProperty] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!id) {
+        return
+      }
+      try {
+        const property = await fetchProp(id)
+        setProperty(property)
+      } catch (error) {
+        console.log('Error fetching property: ', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    if (!property) {
+      fetchData()
+    }
+  }, [id, property])
+}
 
 export default PropertyPage
