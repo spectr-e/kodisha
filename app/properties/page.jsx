0074e1c03095df3be0dd149fea833ae8206b83a6
spectr-e <devs.josia@gmail.com>
2024-05-24 19:36:06 +0300

    fix(PropertiesPage): Import data from mongodb instead of local

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,29 @@
 import { properties } from '@/assets/data'
 import { PropCard } from '@/components'
 
-const PropertiesPage = () => {
+const fetchProps = async () => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`
+    )
+    if (!response.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProps()
+  // sort props by date
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+
   return (
     <section className='px-4 py-6'>
       <div className='px-4 py-6 m-auto container-xl lg:container'>
-        {properties.length === 0 ? (
+        {!properties ? (
           <p>No Properties Found</p>
         ) : (
           <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
