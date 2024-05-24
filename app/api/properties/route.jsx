b3f28031798f0eca739a90234ee81030ceee2906
spectr-e <devs.josia@gmail.com>
2024-05-24 18:52:09 +0300

    fix(api/properties): Edit to fetch data from the database

--- app/api/properties/route.jsx
+++ app/api/properties/route.jsx
@@ -1,9 +1,14 @@
 import connectDB from '@/config/database'
+import Property from '@/model/Property'
 
+// GET /api/properties
 export const GET = async (request) => {
   try {
     await connectDB()
-    return new Response(JSON.stringify({ message: 'Hello Next' }), {
+
+    const properties = await Property.find({})
+
+    return new Response(JSON.stringify(properties), {
       status: 200,
     })
   } catch (error) {
