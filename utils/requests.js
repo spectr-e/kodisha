b8d1a80c918cfb9b31f9cb67e85ae472f7aeceb0
spectr-e <devs.josia@gmail.com>
2024-05-24 19:43:24 +0300

    fix(utils): Handle case where public domain is not available on deployment

--- utils/requests.js
+++ utils/requests.js
@@ -1,14 +1,19 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
+
 const fetchProps = async () => {
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`
-    )
+    // handle domain not available
+    if (!apiDomain) {
+      return []
+    }
+    const response = await fetch(`${apiDomain}/properties`)
     if (!response.ok) {
       throw new Error('Failed to fetch data')
     }
     return response.json()
   } catch (error) {
     console.log(error)
+    return []
   }
 }
 
