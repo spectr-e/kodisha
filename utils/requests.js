f519f1fb31bf6c00c9ca42b4bfe9ceb148ddc1e5
spectr-e <devs.josia@gmail.com>
2024-05-24 20:00:26 +0300

    fix(utils): Fetch single property from database

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
+// GET All properties
 const fetchProps = async () => {
   try {
     // handle domain not available
@@ -17,4 +18,22 @@ const fetchProps = async () => {
   }
 }
 
-export { fetchProps }
+// GET Single Property
+const fetchProp = async (id) => {
+  try {
+    // handle domain not available
+    if (!apiDomain) {
+      return null
+    }
+    const response = await fetch(`${apiDomain}/properties/${id}`)
+    if (!response.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    return response.json()
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export { fetchProps, fetchProp }
