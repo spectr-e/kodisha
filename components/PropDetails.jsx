a62e0e4473d38fc28c0a2e1ae5be8612c2f92ae1
spectr-e <devs.josia@gmail.com>
2024-05-25 14:05:09 +0300

    fix(PropDetails): Make the monthly and weekly rate display dynamic + adding comments

--- components/PropDetails.jsx
+++ components/PropDetails.jsx
@@ -10,6 +10,7 @@ import {
 const PropDetails = ({ property }) => {
   return (
     <main>
+      {/* <!-- Header --> */}
       <div className='bg-white p-6 rounded-lg shadow-md text-center md:text-left'>
         <div className='text-gray-500 mb-4'>{property.type}</div>
         <h1 className='text-3xl font-bold mb-4'>{property.name}</h1>
@@ -21,33 +22,54 @@ const PropDetails = ({ property }) => {
           </p>
         </div>
 
+        {/* <!-- Rates & Options --> */}
         <h3 className='text-lg font-bold my-6 bg-gray-800 text-white p-2'>
           Rates & Options
         </h3>
         <div className='flex flex-col md:flex-row justify-around'>
+          {/* <!-- Nightly --> */}
           <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
             <div className='text-gray-500 mr-2 font-bold'>Nightly</div>
             <div className='text-2xl font-bold'>
               {property.rates.nightly ? (
                 <div className='text-2xl font-bold text-blue-500'>
-                  KES ${property.rates.nightly.toLocaleString()}
+                  KES {property.rates.nightly.toLocaleString()}
                 </div>
               ) : (
                 <FaXmark className='text-red-700' />
               )}
             </div>
           </div>
+          {/* <!-- Weekly --> */}
           <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
             <div className='text-gray-500 mr-2 font-bold'>Weekly</div>
-            <div className='text-2xl font-bold text-blue-500'>$1,100</div>
+            <div className='text-2xl font-bold'>
+              {property.rates.weekly ? (
+                <div className='text-2xl font-bold text-blue-500'>
+                  KES {property.rates.weekly.toLocaleString()}
+                </div>
+              ) : (
+                <FaXmark className='text-red-700' />
+              )}
+            </div>
           </div>
+          {/* <!-- Monthly --> */}
           <div className='flex items-center justify-center mb-4 pb-4 md:pb-0'>
             <div className='text-gray-500 mr-2 font-bold'>Monthly</div>
-            <div className='text-2xl font-bold text-blue-500'>$4,200</div>
+            <div className='text-2xl font-bold'>
+              {property.rates.monthly ? (
+                <div className='text-2xl font-bold text-blue-500'>
+                  KES {property.rates.monthly.toLocaleString()}
+                </div>
+              ) : (
+                <FaXmark className='text-red-700' />
+              )}
+            </div>
           </div>
         </div>
       </div>
 
+      {/* <!-- Description & Details --> */}
       <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
         <h3 className='text-lg font-bold mb-6'>Description & Details</h3>
         <div className='flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9'>
@@ -72,9 +94,9 @@ const PropDetails = ({ property }) => {
         </p>
       </div>
 
+      {/* <!-- Amenities --> */}
       <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
         <h3 className='text-lg font-bold mb-6'>Amenities</h3>
-
         <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none'>
           <li>
             <FaCheck className='text-green-600 mr-2 mt-3' /> Wifi
@@ -131,6 +153,8 @@ const PropDetails = ({ property }) => {
           </li>
         </ul>
       </div>
+
+      {/* <!-- Mapbox --> */}
       <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
         <div id='map'></div>
       </div>
