84fd427b383a089de9a37666d530cddb458d7ec9
spectr-e <devs.josia@gmail.com>
2024-05-25 13:58:54 +0300

    fix(PropDetails): Make the nightly rate display dynamic

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -61,7 +61,8 @@ const PropertyPage = () => {
           <section className='bg-blue-50'>
             <div className='container m-auto py-10 px-6'>
               <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
-                <PropDetails />
+                {/* <!-- Property Details --> */}
+                <PropDetails property={property} />
 
                 {/* <!-- Sidebar --> */}
                 <aside className='space-y-4'>
--- components/PropDetails.jsx
+++ components/PropDetails.jsx
@@ -7,15 +7,18 @@ import {
   FaMapMarker,
 } from 'react-icons/fa'
 
-const PropDetails = () => {
+const PropDetails = ({ property }) => {
   return (
     <main>
       <div className='bg-white p-6 rounded-lg shadow-md text-center md:text-left'>
-        <div className='text-gray-500 mb-4'>Apartment</div>
-        <h1 className='text-3xl font-bold mb-4'>Boston Commons Retreat</h1>
+        <div className='text-gray-500 mb-4'>{property.type}</div>
+        <h1 className='text-3xl font-bold mb-4'>{property.name}</h1>
         <div className='text-gray-500 mb-4 flex align-middle justify-center md:justify-start'>
           <FaMapMarker className='text-lg text-orange-700 mr-2' />
-          <p className='text-orange-700'>120 Tremont Street Boston, MA 02111</p>
+          <p className='text-orange-700'>
+            {property.location.street}, {property.location.city},{' '}
+            {property.location.state}
+          </p>
         </div>
 
         <h3 className='text-lg font-bold my-6 bg-gray-800 text-white p-2'>
@@ -25,7 +28,13 @@ const PropDetails = () => {
           <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
             <div className='text-gray-500 mr-2 font-bold'>Nightly</div>
             <div className='text-2xl font-bold'>
-              <FaXmark className='text-red-700' />
+              {property.rates.nightly ? (
+                <div className='text-2xl font-bold text-blue-500'>
+                  KES ${property.rates.nightly.toLocaleString()}
+                </div>
+              ) : (
+                <FaXmark className='text-red-700' />
+              )}
             </div>
           </div>
           <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
