e30b36b5daaf5c48e135300283f48f73a11da0a9
spectr-e <devs.josia@gmail.com>
2024-06-02 19:35:05 +0300

    fix(authOptions): Add authorization to enable login of different accounts

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -5,6 +5,14 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      // when we click on login, we can choose a google account and not use the same google account logged in before
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
     }),
   ],
 }
