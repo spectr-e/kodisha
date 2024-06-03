a33d83aec90b698c7123f82eb99ba0a2cbbb2b35
spectr-e <devs.josia@gmail.com>
2024-06-03 12:12:21 +0300

    fix(NavBar): Add the ability to signout from account

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -207,6 +207,10 @@ const NavBar = () => {
                       Saved Properties
                     </Link>
                     <button
+                      onClick={() => {
+                        setOpenMobileMenu(false)
+                        signOut()
+                      }}
                       className='block px-4 py-2 text-sm text-gray-700'
                       role='menuitem'
                       tabIndex='-1'
