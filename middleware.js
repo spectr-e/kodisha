cd8590d40452286f0f95615b6d72bcda251cc4ae
spectr-e <devs.josia@gmail.com>
2024-06-03 12:15:09 +0300

    fix(NavBar): Add protection to sensitive routes using middleware

---
+++ middleware.js
@@ -0,0 +1,9 @@
+export { default } from 'next-auth/middleware'
+export const config = {
+  matcher: [
+    '/properties/add',
+    '/profile',
+    '/properties/saved',
+    '/notifications',
+  ],
+}
