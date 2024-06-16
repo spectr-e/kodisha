f84159aca4d16b845a4d4f3819339ed7abf90a72
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-16 23:28:08 +0300

    fix(ProfilePage): Make the properties displayed dynamic

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,15 +1,48 @@
 'use client'
 
 import { profile } from '@/assets/images'
+import { Spinner } from '@/components'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 const ProfilePage = () => {
   const { data: session } = useSession()
   const profileImg = session?.user?.image
   const name = session?.user?.name
   const email = session?.user?.email
+
+  const [props, setProps] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const userProps = async (id) => {
+      if (!id) {
+        return
+      }
+
+      try {
+        const resp = await fetch(`api/properties/user/${id}`, {
+          next: { revalidate: 60 },
+        })
+
+        if (resp.status === 200) {
+          const data = await resp.json()
+          setProps(data)
+        }
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    // fetch user properties when session is available
+    if (session?.user?.id) {
+      userProps(session.user.id)
+    }
+  }, [session])
 
   return (
     <section className='bg-blue-50'>
@@ -35,68 +68,48 @@ const ProfilePage = () => {
               </h2>
             </div>
 
+            {/* <!-- Listings Section --> */}
             <div className='md:w-3/4 md:pl-4'>
               <h2 className='mb-4 text-xl font-semibold'>Your Listings</h2>
-              <div className='mb-10'>
-                <Link href='/property.html'>
-                  <Image
-                    sizes='100vh'
-                    height={0}
-                    width={0}
-                    className='object-cover w-full h-32 rounded-md'
-                    src='/images/properties/a1.jpg'
-                    alt='Property 1'
-                  />
-                </Link>
-                <div className='mt-2'>
-                  <p className='text-lg font-semibold'>Property Title 1</p>
-                  <p className='text-gray-600'>Address: 123 Main St</p>
-                </div>
-                <div className='mt-2'>
-                  <Link
-                    href='/add-property.html'
-                    className='px-3 py-3 mr-2 text-white bg-blue-500 rounded-md hover:bg-blue-600'
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    className='px-3 py-2 text-white bg-red-500 rounded-md hover:bg-red-600'
-                    type='button'
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-              <div className='mb-10'>
-                <Link href='/property.html'>
-                  <Image
-                    sizes='100vh'
-                    height={0}
-                    width={0}
-                    className='object-cover w-full h-32 rounded-md'
-                    src='/images/properties/b1.jpg'
-                    alt='Property 2'
-                  />
-                </Link>
-                <div className='mt-2'>
-                  <p className='text-lg font-semibold'>Property Title 2</p>
-                  <p className='text-gray-600'>Address: 456 Elm St</p>
-                </div>
-                <div className='mt-2'>
-                  <Link
-                    href='/add-property.html'
-                    className='px-3 py-3 mr-2 text-white bg-blue-500 rounded-md hover:bg-blue-600'
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    className='px-3 py-2 text-white bg-red-500 rounded-md hover:bg-red-600'
-                    type='button'
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
+              {!loading && props.length === 0 && (
+                <p>You have no property listings</p>
+              )}
+              {loading ? (
+                <Spinner loading={loading} />
+              ) : (
+                props.map((prop) => (
+                  <div key={prop._id} className='mb-10'>
+                    <Link href={`/properties/${prop._id}`}>
+                      <Image
+                        height={100}
+                        width={500}
+                        className='object-cover w-full h-32 rounded-md'
+                        src={prop.images[0]}
+                        priority={true}
+                        alt='Property 1'
+                      />
+                    </Link>
+                    <div className='mt-2'>
+                      <p className='text-lg font-semibold'>{prop.name}</p>
+                      <p className='text-gray-600'>{prop.location.street}</p>
+                    </div>
+                    <div className='mt-2'>
+                      <Link
+                        href='/add-property.html'
+                        className='px-3 py-3 mr-2 text-white bg-blue-500 rounded-md hover:bg-blue-600'
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        className='px-3 py-2 text-white bg-red-500 rounded-md hover:bg-red-600'
+                        type='button'
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                ))
+              )}
             </div>
           </div>
         </div>
