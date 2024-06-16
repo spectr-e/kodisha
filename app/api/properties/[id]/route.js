cbf488673dfb1a63c425c55c5b22bc4ec44d1de2
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-16 23:42:41 +0300

    fix(api/properties): Create a DELETE api route for properties

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,5 +1,6 @@
 import connectDB from '@/config/database'
 import Property from '@/model/Property'
+import { getSessionUser } from '@/utils/getSessionUser'
 
 // GET /api/properties/:id
 export const GET = async (request, { params }) => {
@@ -12,6 +13,45 @@ export const GET = async (request, { params }) => {
     }
     // successful reponse
     return new Response(JSON.stringify(property), {
+      status: 200,
+    })
+  } catch (error) {
+    console.log(error)
+    return new Response('Something is not right', { status: 500 })
+  }
+}
+
+// DELETE /api/properties/:id
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectDB()
+
+    const sessionUser = await getSessionUser()
+
+    // check for session
+    if (!sessionUser || sessionUser.userId) {
+      return new Response('Unauthorized', { status: 401 })
+    }
+
+    const { userId } = sessionUser
+    const propId = params.id
+
+    const prop = await Property.findById(propId)
+
+    // unsuccessful reponse
+    if (!prop) {
+      return new Response('Property not found', { status: 404 })
+    }
+
+    // verify owner
+    if (prop.owner.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 })
+    }
+
+    await prop.deleteOne()
+
+    // successful reponse
+    return new Response('Property deleted!', {
       status: 200,
     })
   } catch (error) {
