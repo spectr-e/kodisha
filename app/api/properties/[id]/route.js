21cd9eea560bd0fa6e1cf91179ee154a65eeccc6
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-17 09:50:31 +0300

    fix(api/properties/id): Add a PUT request function to the api route

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -58,3 +58,75 @@ export const DELETE = async (request, { params }) => {
     return new Response('Something is not right', { status: 500 })
   }
 }
+
+// PUT /api/properties/:id
+export const PUT = async (req, { params }) => {
+  try {
+    // a. connect to db
+    await connectDB()
+
+    // b. get the userid to attach to property
+    const sessionUser = await getSessionUser()
+    const { userId } = sessionUser
+    // if not authorized
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('Not authorized - User ID required!', { status: 401 })
+    }
+
+    // property id
+    const { id } = params.id
+
+    // c. get the form data
+    const formData = await req.formData()
+
+    // d. access all values from amenities
+    const amenities = formData.getAll('amenities')
+
+    // get property to update
+    const existingProperty = await Property.findById(id)
+
+    if (!existingProperty) {
+      return new Response('Property does not exist!', { status: 404 })
+    }
+
+    // verify ownership
+    if (existingProperty.owner.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 })
+    }
+
+    // e. create propertydata object for submission
+    const propData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.city'),
+        state: formData.get('location.state'),
+        zip: formData.get('location.zip'),
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      sq: formData.get('sq'),
+      amenities,
+      rates: {
+        nightly: formData.get('rates.nightly'),
+        weekly: formData.get('rates.weekly'),
+        monthly: formData.get('rates.monthly'),
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        email: formData.get('seller_info.email'),
+        phone: formData.get('seller_info.phone'),
+      },
+      owner: userId,
+    }
+
+    // g. update prop in db
+    const updatedProp = await Property.findByIdAndUpdate(id, propData)
+    return new Response(JSON.stringify(updatedProp), { status: 200 })
+  } catch (error) {
+    console.log(error)
+    return new Response('Failed to add property', { status: 500 })
+  }
+}
