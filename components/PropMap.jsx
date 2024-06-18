79e502a2eae9c6186c428e01172bf4ed21423eab
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-19 00:06:54 +0300

    fix: Remove react geocode dependency

--- components/PropMap.jsx
+++ components/PropMap.jsx
@@ -1,4 +1,24 @@
-const PropMap = () => {
+'use client'
+import { useEffect, useState } from 'react'
+import { Map, Marker } from 'react-map-gl'
+import { Spinner } from '.'
+import { pin } from '@/assets/images'
+import Image from 'next/image'
+import geocoder from 'geocoder'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const PropMap = ({ property }) => {
+  const [lat, setLat] = useState(null)
+  const [lng, setLng] = useState(null)
+  const [viewPort, setViewPort] = useState({
+    latitude: 0,
+    longitude: 0,
+    zoom: 12,
+    width: '100%',
+    height: '500px',
+  })
+  const [loading, setLoading] = useState(true)
+
   return <div></div>
 }
 
