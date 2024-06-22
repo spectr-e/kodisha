8b63e771817e8b8cbd72f70d45ad4c1a12d1a9c6
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-22 16:51:28 +0300

    fix(api/bookmarks): Create a get request for the bookmarks

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -4,6 +4,7 @@ import { getSessionUser } from '@/utils/getSessionUser'
 
 export const dynamic = 'force-dynamic'
 
+// POST /aoi/bookmarks
 export const POST = async (request) => {
   try {
     await connectDB()
@@ -43,6 +44,33 @@ export const POST = async (request) => {
     })
   } catch (err) {
     console.log(err)
+    return new Response('Something is not right', { status: 500 })
+  }
+}
+
+// GET /aoi/bookmarks
+export const GET = async (req) => {
+  try {
+    await connectDB()
+    // get user details
+    const sessionUser = await getSessionUser()
+    const { userId } = sessionUser
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('Not authorized - User ID required!', {
+        status: 401,
+      })
+    }
+
+    // find user in db
+    const user = await User.findById(userId)
+
+    // get bookmarks from user and their associated properties details
+    const bookmarks = await Property.find({ id: { $in: user.bookmarks } })
+    return new Response(JSON.stringify({ bookmarks }), {
+      status: 200,
+    })
+  } catch (error) {
+    console.log(error)
     return new Response('Something is not right', { status: 500 })
   }
 }
