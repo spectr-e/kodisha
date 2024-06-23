71678627f39d3c678f38cc35f2a55c5bb8d7a3e2
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-23 10:57:09 +0300

    fix(ShareBtn): Add react share to handle sharing function

--- components/ShareBtn.jsx
+++ components/ShareBtn.jsx
@@ -1,10 +1,58 @@
 import { FaShare } from 'react-icons/fa'
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  LinkedinShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  EmailIcon,
+  WhatsappIcon,
+  LinkedinIcon,
+} from 'react-share'
 
 const ShareBtn = ({ property }) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`
+
   return (
-    <button className='flex items-center justify-center w-full px-4 py-2 font-bold text-white bg-orange-500 rounded-full hover:bg-orange-600'>
-      <FaShare className='mr-2' /> Share Property
-    </button>
+    <>
+      {' '}
+      <h3 className='pt-2 text-xl font-bold text-center'>
+        Share This Property
+      </h3>
+      <div className='flex justify-center gap-3 pb-5'>
+        <FacebookShareButton
+          url={shareUrl}
+          quote={property.name}
+          hashtag={`#${property.type.replace(/\s/g, '')}ForRent`}
+        >
+          <FacebookIcon size={40} round={true} />
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={shareUrl}
+          quote={property.name}
+          hashtags={[`${property.type.replace(/\s/g, '')}ForRent`]}
+        >
+          <TwitterIcon size={40} round={true} />
+        </TwitterShareButton>
+        <WhatsappShareButton
+          url={shareUrl}
+          quote={property.name}
+          seperator=':: '
+        >
+          <WhatsappIcon size={40} round={true} />
+        </WhatsappShareButton>
+        <EmailShareButton
+          url={shareUrl}
+          quote={property.name}
+          seperator=':: '
+          body={`Check out this property listing: ${shareUrl}`}
+        >
+          <EmailIcon size={40} round={true} />
+        </EmailShareButton>
+      </div>
+    </>
   )
 }
 
