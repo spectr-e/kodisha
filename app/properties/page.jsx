88ead8c9037a6a3d2dc5b18b79cff72cb0daa5b0
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-23 14:14:40 +0300

    fix(Properties): Add a search box in the property listing page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,4 +1,5 @@
-import { PropCard } from '@/components'
+'use client'
+import { PropCard, SearchForm } from '@/components'
 import { fetchProps } from '@/utils/requests'
 
 const PropertiesPage = async () => {
@@ -7,19 +8,27 @@ const PropertiesPage = async () => {
   properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
 
   return (
-    <section className='px-4 py-6'>
-      <div className='px-4 py-6 m-auto container-xl lg:container'>
-        {!properties ? (
-          <p>No Properties Found</p>
-        ) : (
-          <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
-            {properties.map((prop, i) => (
-              <PropCard property={prop} key={i} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className='py-4 bg-blue-700'>
+        <div className='flex flex-col items-start px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
+          <SearchForm />
+        </div>
+      </section>
+
+      <section className='px-4 py-6'>
+        <div className='px-4 py-6 m-auto container-xl lg:container'>
+          {!properties ? (
+            <p>No Properties Found</p>
+          ) : (
+            <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
+              {properties.map((prop, i) => (
+                <PropCard property={prop} key={i} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   )
 }
 
