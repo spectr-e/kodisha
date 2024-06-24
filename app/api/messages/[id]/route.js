62ab91086f592274f97f0ac63538fcf6dfeffb25
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-24 16:13:12 +0300

    fix(/api/messages/:id): Create the delete route api

--- app/api/messages/[id]/route.js
+++ app/api/messages/[id]/route.js
@@ -5,6 +5,50 @@ import { getSessionUser } from '@/utils/getSessionUser'
 // to avoid deployment issues
 export const dynamic = 'force-dynamic'
 
+// DELETE /api/messages/:id
+export const DELETE = async (req, { params }) => {
+  try {
+    // a. connect to db
+    await connectDB()
+
+    // b. get the userid to attach to property
+    const sessionUser = await getSessionUser()
+    const { user } = sessionUser
+
+    // if not authorized
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response(
+        JSON.stringify({ message: 'Not authorized - Please log in!' }),
+        { status: 401 }
+      )
+    }
+    // c. messageId
+    const { id } = params
+
+    // d. find message by id
+    const message = await Message.findById(id)
+
+    // f. handle not found message
+    if (!message) {
+      return new Response('Message not found!', { status: 404 })
+    }
+
+    // e. check if user owns message
+    if (message.recipient.toString() !== user.id) {
+      return new Response('Unauthorized', { status: 401 })
+    }
+    // f. delete message
+    await message.deleteOne()
+
+    // g. return
+    return new Response('Message deleted successfully!', {
+      status: 200,
+    })
+  } catch (error) {
+    console.log(error)
+    return new Response('Something went wrong!', { status: 500 })
+  }
+}
 // PUT /api/messages/:id
 export const PUT = async (req, { params }) => {
   try {
