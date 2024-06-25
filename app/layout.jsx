45c26a8334a417985ed836ae14da11eef33e5cb6
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-25 10:16:23 +0300

    fix(PropImages): Wrap the whole component with PhotoSwipe gallery

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { AuthProvider, Footer, NavBar } from '@/components'
 import { ToastContainer } from 'react-toastify'
 import { GlobalProvider } from '@/context/GlobalContext'
 import 'react-toastify/dist/ReactToastify.css'
+import 'photoswipe/dist/photoswipe.css'
 
 export const metadata = {
   title: 'Kodisha | Find The Perfect Rental',
