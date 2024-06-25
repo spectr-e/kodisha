b90075bea73e83b6defdb908bb642d12718e1b1c
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-25 10:25:43 +0300

    fix(PropImages): Add the photoswipe dependency attributes

--- components/PropImages.jsx
+++ components/PropImages.jsx
@@ -7,14 +7,25 @@ const PropImages = ({ images }) => {
       <section className='p-4 bg-blue-50'>
         <div className='container mx-auto'>
           {images !== undefined && images.length === 1 ? (
-            <Image
-              src={images[0]}
-              alt=''
-              className='object-cover mx-auto rounded-xl'
-              width={1800}
-              height={400}
-              priority={true}
-            />
+            <Item
+              original={images[0]}
+              thumbnail={images[0]}
+              width='1024'
+              height='768'
+            >
+              {({ ref, open }) => (
+                <Image
+                  src={images[0]}
+                  alt=''
+                  className='object-cover mx-auto rounded-xl'
+                  width={1800}
+                  height={400}
+                  priority={true}
+                  ref={ref}
+                  onClick={open}
+                />
+              )}
+            </Item>
           ) : (
             <div className='grid grid-cols-2 gap-4'>
               {images !== undefined &&
@@ -25,14 +36,23 @@ const PropImages = ({ images }) => {
               `}
                     key={i}
                   >
-                    <Image
-                      src={image}
-                      alt=''
-                      className='object-cover mx-auto rounded-xl'
-                      width={1800}
-                      height={400}
-                      priority={true}
-                    />
+                    <Item
+                      original={image}
+                      thumbnail={image}
+                      width='1024'
+                      height='768'
+                    >
+                      {({ ref, open }) => (
+                        <Image
+                          src={image}
+                          alt=''
+                          className='object-cover mx-auto rounded-xl'
+                          width={1800}
+                          height={400}
+                          priority={true}
+                        />
+                      )}
+                    </Item>
                   </div>
                 ))}
             </div>
