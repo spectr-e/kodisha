55ac82223570a802490debcc2cf592a407be7cf4
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-06-25 11:17:48 +0300

    fix(Featured): Make the section disappear if no featured properties

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -28,19 +28,21 @@ const Featured = () => {
   }, [])
 
   return (
-    <section className='bg-blue-50 px-4 pt-6 pb-10'>
-      <div className='container-xl lg:container m-auto'>
-        <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
-          Featured Properties
-        </h2>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-          {featuredProps.length > 0 &&
-            featuredProps.map((feature, i) => {
-              return <FeaturedCard key={i} property={feature} />
-            })}
+    featuredProps.length > 0 && (
+      <section className='bg-blue-50 px-4 pt-6 pb-10'>
+        <div className='container-xl lg:container m-auto'>
+          <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
+            Featured Properties
+          </h2>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+            {featuredProps.length > 0 &&
+              featuredProps.map((feature, i) => {
+                return <FeaturedCard key={i} property={feature} />
+              })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    )
   )
 }
 
